7cae60af33dabc3f5e0c6380e6de9b4f8039746b
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-07 23:52:50 -0300

    :biking_woman: Commit inicial Blog de Receitas

---
+++ src/main/java/com/audrey/blogreceitas/blogReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.audrey.blogreceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class blogReceitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(blogReceitasApplication.class, args);
+    }
+
+}
