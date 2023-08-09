399ada432674ab9ece3cfec555e2d2926ed1cb58
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-09 20:36:17 -0300

    :kick_scooter: Adição de endpoint na classe controller e método getAll

--- src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
+++ src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
@@ -1,11 +1,16 @@
 package com.audrey.blogreceitas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.audrey.blogreceitas.dao.ReceitaDAO;
+import com.audrey.blogreceitas.repository.ReceitaRepository;
+import com.audrey.blogreceitas.model.Receita;
 
 @RestController
 @RequestMapping("/receitas")
@@ -13,7 +18,13 @@ import com.audrey.blogreceitas.dao.ReceitaDAO;
 public class ReceitaController {
 
     @Autowired
-    private ReceitaDAO  minhaReceitaDao;
+    private ReceitaRepository bananinhaRepository ;
+
+    @GetMapping
+    public ResponseEntity<List<Receita>> getAll(){
+
+        return ResponseEntity.ok(bananinhaRepository.findAll());
+    }
 
     
 }
