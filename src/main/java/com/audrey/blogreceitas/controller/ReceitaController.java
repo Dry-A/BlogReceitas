12465d601c9b644d8343e5904f9ac8963123d904
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-11 19:42:33 -0300

    :yo_yo: Implementando método Post

--- src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
+++ src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
@@ -1,16 +1,18 @@
 package com.audrey.blogreceitas.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.audrey.blogreceitas.repository.ReceitaRepository;
+import jakarta.validation.Valid;
 import com.audrey.blogreceitas.model.Receita;
 
 @RestController
@@ -40,5 +42,10 @@ public class ReceitaController {
 
         return ResponseEntity.ok(bananinhaRepository.findAllByTituloContainingIgnoreCase(titulo));
     }
+@PostMapping
+	public ResponseEntity<Receita> post(@Valid @RequestBody Receita receita) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(bananinhaRepository.save(receita));
+	}
 
 }
