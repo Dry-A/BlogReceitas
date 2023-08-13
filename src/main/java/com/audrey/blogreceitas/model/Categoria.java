bf44b93831b17c96103974f7bd88553aa76391e7
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-13 16:44:53 -0300

    :weight_lifting_woman: Criaçao camada Categoria

---
+++ src/main/java/com/audrey/blogreceitas/model/Categoria.java
@@ -0,0 +1,31 @@
+package com.audrey.blogreceitas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome da Categoria é Obrigatório!!!")
+    private String nome;
+
+    
+    
+}
