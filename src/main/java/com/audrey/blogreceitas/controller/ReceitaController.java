5ee0ee696147a7ebd12f78fd7b6598968e57edbe
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-13 14:52:44 -0300

    :zombie_woman: Adição metodo delete

--- src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
+++ src/main/java/com/audrey/blogreceitas/controller/ReceitaController.java
@@ -1,17 +1,23 @@
 package com.audrey.blogreceitas.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.audrey.blogreceitas.repository.ReceitaRepository;
 import jakarta.validation.Valid;
 import com.audrey.blogreceitas.model.Receita;
@@ -57,6 +63,20 @@ public class ReceitaController {
                 .map(resposta -> ResponseEntity.status(HttpStatus.OK)
                         .body(bananinhaRepository.save(receita)))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+      
+        Optional<Receita> receita = bananinhaRepository.findById(id);
+
+        if (receita.isEmpty())
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        bananinhaRepository.deleteById(id);
+
     }
 
 }
